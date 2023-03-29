78f65e1e661a3bf705f4a9511ed764910c133273
Quentyn May <mayq@wit.edu>
2023-03-29 10:41:36 -0400

    Add files via upload

---
+++ src/Clicker.java
@@ -0,0 +1,79 @@
+import javafx.scene.control.Button;
+
+/**
+ * Clicker class
+ * @author Quentyn May
+ *
+ */
+public class Clicker implements Buildable {
+
+	private static double PPS = 0.1;
+	private static double cost = 15.0;
+	private static final double BASE_COST = 15.0;
+	private static int numClickers = 0;
+	private static double myClickerPPS = 0.0;
+	private static Button graphic = new Button(String.format("%d Clickers%nCost: %,.0f", numClickers, cost));
+	
+    public Clicker() {
+    	buyBuilding();
+    }
+
+	@Override
+	public void buyBuilding() {		
+		Player.updatePretzels(-Clicker.getCost());
+
+		Clicker.setNumClickers(1);
+		Player.updateBuildings(1);
+
+		Clicker.setMyClickerPPS(Clicker.getPPS());
+		Player.updatePPS(Clicker.getPPS());
+
+		Clicker.setCost((double) Math.round(BASE_COST * Math.pow(1.15, numClickers)));
+		Clicker.setGraphic(String.format("%d Clickers%nCost: %,.0f", numClickers, cost));
+	}
+
+	@Override
+	public void upgrade() {
+	}
+
+	public static double getPPS() {
+		return PPS;
+	}
+
+	public static void setPPS(double d) {
+		PPS = d;
+	}
+
+	public static double getCost() {
+		return cost;
+	}
+
+	public static void setCost(double cost) {
+		Clicker.cost = cost;
+	}
+
+	public static int getNumClickers() {
+		return numClickers;
+	}
+
+	public static void setNumClickers(int num) {
+		Clicker.numClickers += num;
+	}
+
+	public static double getMyClickerPPS() {
+		return myClickerPPS;
+	}
+
+	public static void setMyClickerPPS(double d) {
+		Clicker.myClickerPPS += d;
+	}
+
+	public static Button getGraphic() {
+		return graphic;
+	}
+
+	public static void setGraphic(String text) {
+		graphic.setText(text);
+	}
+
+}
